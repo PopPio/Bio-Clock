31dbc1863eb95c25a4dac2bfe8fe6ea8ff555c08
PopPio <pop_sg88@hotmail.com>
2012-11-14 15:03:22 +0700

    move info string method to global variable class

--- src/com/poppio/bioclock/GlobalVar.java
+++ src/com/poppio/bioclock/GlobalVar.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 public class GlobalVar extends Application {
     private boolean check = false;
+    private Information [] info;
 
     public boolean isCheck() {
             return check;
@@ -13,9 +14,33 @@ public class GlobalVar extends Application {
     public void setCheck(boolean value) {
             this.check = value;
     }
+    
+    public Information [] getInfo(){
+    	return info;
+    }
+    
     @Override
     public void onCreate() {
             //reinitialize variable
     	Log.d("PopPio", "Global Var onCreate()");
+    	info = new Information [13];
+    	fetchInfo ();
     }
+    
+    
+    private void fetchInfo (){
+		info[0] = new Information(0, 0, 0, 0);
+		info[1] = new Information(R.string.time1, R.string.info1, R.string.infoEx1, R.string.noti1);
+		info[2] = new Information(R.string.time2, R.string.info2, R.string.infoEx2, R.string.noti2);
+		info[3] = new Information(R.string.time3, R.string.info3, R.string.infoEx3, R.string.noti3);
+		info[4] = new Information(R.string.time4, R.string.info4, R.string.infoEx4, R.string.noti4);
+		info[5] = new Information(R.string.time5, R.string.info5, R.string.infoEx5, R.string.noti5);
+		info[6] = new Information(R.string.time6, R.string.info6, R.string.infoEx6, R.string.noti6);
+		info[7] = new Information(R.string.time7, R.string.info7, R.string.infoEx7, R.string.noti7);
+		info[8] = new Information(R.string.time8, R.string.info8, R.string.infoEx8, R.string.noti8);
+		info[9] = new Information(R.string.time9, R.string.info9, R.string.infoEx9, R.string.noti9);
+		info[10] = new Information(R.string.time10, R.string.info10, R.string.infoEx10, R.string.noti10);
+		info[11] = new Information(R.string.time11, R.string.info11, R.string.infoEx11, R.string.noti11);
+		info[12] = new Information(R.string.time12, R.string.info12, R.string.infoEx12, R.string.noti12);
+	}
 }
--- src/com/poppio/bioclock/Info_screen.java
+++ src/com/poppio/bioclock/Info_screen.java
@@ -18,24 +18,24 @@ public class Info_screen extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
-		fetchInfo ();
-		
+		//get current timeID from home screen
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
 			currentId = extras.getInt("timeID");
 		}
 		
+		//get string info from global variable
+		info = ((GlobalVar)getApplication()).getInfo();
+		
+		//set text
 		int infoTextId = info[currentId].getDescription();
 		int timeTestId = info[currentId].getTime();
 		int extraId = info[currentId].getExtra();
 		setContentView(R.layout.info_activity);
-		
 		TextView info = (TextView) findViewById(R.id.infoText);
 		info.setText(infoTextId);
-		
 		TextView time = (TextView) findViewById(R.id.timeText);
 		time.setText(timeTestId);
-		
 		TextView extra = (TextView) findViewById(R.id.extraText);
 		if(getString(extraId).equals("null")){
 			extra.setVisibility(View.GONE);
@@ -43,17 +43,16 @@ public class Info_screen extends Activity {
 			extra.setText(extraId);
 		}
 		
-		
+		//set back button
 		ImageButton back = (ImageButton) findViewById(R.id.backButton);
 		back.setOnClickListener(new OnClickListener() {
-
 			public void onClick(View v) {
 				Log.d("PopPio", "backButton was clicked");
 				startActivity(new Intent(Info_screen.this, MainActivity.class));
 			}
 		});
 		
-		
+		//set checkbox
 		final ImageButton check = (ImageButton) findViewById(R.id.checkInfo);
 		if(((GlobalVar)getApplication()).isCheck()){
 			check.setImageResource(R.drawable.circle_check);
@@ -75,25 +74,7 @@ public class Info_screen extends Activity {
 				}
 			}
 		});
+		
 	}
 	
-	private void fetchInfo (){
-		info[0] = new Information(0, 0, 0, 0);
-		info[1] = new Information(R.string.time1, R.string.info1, R.string.infoEx1, R.string.noti1);
-		info[2] = new Information(R.string.time2, R.string.info2, R.string.infoEx2, R.string.noti2);
-		info[3] = new Information(R.string.time3, R.string.info3, R.string.infoEx3, R.string.noti3);
-		info[4] = new Information(R.string.time4, R.string.info4, R.string.infoEx4, R.string.noti4);
-		info[5] = new Information(R.string.time5, R.string.info5, R.string.infoEx5, R.string.noti5);
-		info[6] = new Information(R.string.time6, R.string.info6, R.string.infoEx6, R.string.noti6);
-		info[7] = new Information(R.string.time7, R.string.info7, R.string.infoEx7, R.string.noti7);
-		info[8] = new Information(R.string.time8, R.string.info8, R.string.infoEx8, R.string.noti8);
-		info[9] = new Information(R.string.time9, R.string.info9, R.string.infoEx9, R.string.noti9);
-		info[10] = new Information(R.string.time10, R.string.info10, R.string.infoEx10, R.string.noti10);
-		info[11] = new Information(R.string.time11, R.string.info11, R.string.infoEx11, R.string.noti11);
-		info[12] = new Information(R.string.time12, R.string.info12, R.string.infoEx12, R.string.noti12);
-	}
-
-	// public void stop(){
-	// super.onStop();
-	// }
 }
